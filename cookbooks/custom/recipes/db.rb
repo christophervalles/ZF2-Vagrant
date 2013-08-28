956895dd26b434bcfdc5500cec3b4d008118db95
Christopher Valles <info@christophervalles.com>
2013-08-28 12:49:37 +0100

    Database will not be populated by default

--- cookbooks/custom/recipes/db.rb
+++ cookbooks/custom/recipes/db.rb
@@ -4,8 +4,3 @@ execute "Create database" do
   creates "/home/vagrant/.db_created"
   action :run
 end
-
-execute "Import database" do
-  command "if [ -f /var/source-api/data/db/sn.sql ]; then mysql -uroot -proot sn < /var/source-api/data/db/sn.sql; fi"
-  action :run
-end
